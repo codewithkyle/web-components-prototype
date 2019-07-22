0f6988ec935e8982be9d6ed548b44a6ca71de6ae
Kyle Andrews <kylea@page.works>
2019-07-22 08:41:29 -0400

    Styles line item component

--- docs/add-line-item-component.ts
+++ docs/add-line-item-component.ts
@@ -2,12 +2,14 @@ class AddLineItemComponent extends HTMLElement
 {
     private _lineItemTemplate : HTMLTemplateElement;
     private _cartComponent : HTMLElement;
+    private _lineItemsContainer : HTMLElement;
 
     constructor()
     {
         super();
         this._lineItemTemplate = document.body.querySelector('.js-line-item-template');
         this._cartComponent = document.body.querySelector('cart-component');
+        this._lineItemsContainer = this._cartComponent.querySelector('cart-line-items');
     }
 
     private handleClick:EventListener = this.addLineItem.bind(this);
@@ -20,7 +22,7 @@ class AddLineItemComponent extends HTMLElement
     private addLineItem() : void
     {
         const node = document.importNode(this._lineItemTemplate.content, true);
-        this._cartComponent.appendChild(node);
+        this._lineItemsContainer.appendChild(node);
     }
 }
 
--- docs/assets/add-line-item-component.js
+++ docs/assets/add-line-item-component.js
@@ -4,14 +4,14 @@ class AddLineItemComponent extends HTMLElement {
         this.handleClick = this.addLineItem.bind(this);
         this._lineItemTemplate = document.body.querySelector('.js-line-item-template');
         this._cartComponent = document.body.querySelector('cart-component');
+        this._lineItemsContainer = this._cartComponent.querySelector('cart-line-items');
     }
     connectedCallback() {
         this.addEventListener('click', this.handleClick);
     }
     addLineItem() {
         const node = document.importNode(this._lineItemTemplate.content, true);
-        console.log(node);
-        this._cartComponent.appendChild(node);
+        this._lineItemsContainer.appendChild(node);
     }
 }
 customElements.define('add-line-item-component', AddLineItemComponent);
