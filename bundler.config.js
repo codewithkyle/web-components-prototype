d0c2fac7b18cffb1dd22a60160ffe1f87b7745b3
Kyle Andrews <kylea@page.works>
2019-07-23 19:43:18 -0400

    Adds basic node module bundling

--- bundler.config.js
+++ bundler.config.js
@@ -1,16 +1,113 @@
 const fs = require('fs');
 const glob = require("glob").Glob;
 const rollup = require('rollup');
+const rollupPluginNodeResolve = require('rollup-plugin-node-resolve');
+const rimraf = require('rimraf');
 
-glob('./docs/assets/es6/*.js', (err, files)=>{
-    if(err)
-    {
-        throw err;
-    }
-    getRequiredPackages(files);
-});
+function run()
+{
+    (async ()=>{
+        try
+        {
+            await removeBundles();
+            await removePackages();
+            await makeBundleDirectory();
+            await makePackageDirectory();
+            const files = await getFiles();
+            await buildPackages(files);
+        }
+        catch(error)
+        {
+            throw error;
+        }
+    })();
+}
 
-function getRequiredPackages(files)
+function removeBundles()
+{
+    return new Promise((resolve, reject)=>{
+        if(fs.existsSync('./_bundles'))
+        {
+            rimraf('./_bundles', (error)=>{
+                if(error)
+                {
+                    reject(error);
+                }
+
+                resolve();
+            });
+        }
+        else
+        {
+            resolve();
+        }
+    });
+}
+
+function removePackages()
+{
+    return new Promise((resolve, reject)=>{
+        if(fs.existsSync('./docs/assets/packages'))
+        {
+            rimraf('./docs/assets/packages', (error)=>{
+                if(error)
+                {
+                    reject(error);
+                }
+
+                resolve();
+            });
+        }
+        else
+        {
+            resolve();
+        }
+    });
+}
+
+function makeBundleDirectory()
+{
+    return new Promise((resolve, reject)=>{
+        fs.mkdir('./_bundles', (err)=>{
+            if(err)
+            {
+                reject(err);
+            }
+    
+            resolve();
+        });
+    });
+}
+
+function makePackageDirectory()
+{
+    return new Promise((resolve, reject)=>{
+        fs.mkdir('./docs/assets/packages', (err)=>{
+            if(err)
+            {
+                reject(err);
+            }
+    
+            resolve();
+        });
+    });
+}
+
+function getFiles()
+{
+    return new Promise((resolve, reject)=>{
+        glob('./docs/assets/es6/*.js', (err, files)=>{
+            if(err)
+            {
+                reject(err);
+            }
+        
+            resolve(files);
+        });
+    });
+}
+
+function buildPackages(files)
 {
     for (let i = 0; i < files.length; i++)
     {
@@ -21,42 +118,55 @@ function getRequiredPackages(files)
                 return;
             }
 
-            const requireStatements = data.toString().match(/(require.*?\;)/g);
+            // const requireStatements = data.toString().match(/(require.*?\;)/g);
+            const requireStatements = data.toString().match(/((import[\s\t]).*?\;)/g);
             
             if(requireStatements)
             {
-                fs.mkdir('./bundles', (err)=>{
-                    if(err)
+                for (let k = 0; k < requireStatements.length; k++)
                     {
-                        throw err;
-                    }
+                        let packageName = requireStatements[k].match(/(?<=[\"\'])(.*?)(?=[\"\'])/)[0];
+                        packageName = packageName.replace(/\@.*?\//, '');
 
-                    for (let k = 0; k < requireStatements.length; k++)
-                    {
-                        let packageName = requireStatements[k].match(/(?<=\")(.*?)(?=\")/)[0];
-                        packageName.replace(/\@.*?\//, '');
+                        let importName = requireStatements[k].match(/(?<=import).*(?=from)/)[0];
+                        importName = importName.replace(/(\*\sas)/, '');
+                        importName = importName.replace(/[\{\}]/g, '');
+                        importName = importName.replace(/[\s\t]/g, '');
 
-                        const data = requireStatements[i];
+                        let data = requireStatements[k];
+                        data += `\nconsole.log(${ importName })`;
 
-                        fs.writeFile(`./bundles/${ packageName }.js`, data, (err)=>{
-                            if(eff)
+                        fs.writeFile(`./_bundles/${ packageName }.js`, data, (err)=>{
+                            if(err)
                             {
-                                throw err;
+                                console.log(err);
+                                return;
                             }
+
+                            const inputOptions = {
+                                input: `./_bundles/${ packageName }.js`,
+                                plugins: [
+                                    rollupPluginNodeResolve({
+                                        extensions: [ '.mjs', '.cjs', '.js', '.json' ]
+                                    })
+                                ]
+                            };
+                            const outputOptions = {
+                                file: `./docs/assets/packages/${ packageName }.js`,
+                                format: 'iife'
+                            };
+                            build(inputOptions, outputOptions);
                         });
-                        const rollupOptions = {
-                            input: './bundles/${ packageName }.js'
-                        };
-                        build(rollupOptions);
                     }
-                });
             }
         });
     }
 }
 
-async function build(rollupOptions)
+async function build(inputOptions, outputOptions)
 {
-    const bundle = await rollup.rollup(rollupOptions);
-    console.log(bundle);
+    const bundle = await rollup.rollup(inputOptions);
+    await bundle.write(outputOptions);
 }
+
+run();
--- docs/assets/es6/remove-cart-component.js
+++ docs/assets/es6/remove-cart-component.js
@@ -1,7 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const notifyjs_1 = require("@codewithkyle/notifyjs");
-const animejs_1 = require("animejs");
+import { Notify } from '@codewithkyle/notifyjs';
+import anime from 'animejs';
 class RemoveCartComponent extends HTMLElement {
     constructor() {
         super();
@@ -13,8 +11,8 @@ class RemoveCartComponent extends HTMLElement {
     removeCartComponent() {
         const cart = document.body.querySelector('cart-component');
         cart.remove();
-        new notifyjs_1.Notify({ message: 'Cart was removed successfully' });
-        console.log(animejs_1.default);
+        new Notify({ message: 'Cart was removed successfully' });
+        console.log(anime);
     }
 }
 customElements.define('remove-cart-component', RemoveCartComponent);
