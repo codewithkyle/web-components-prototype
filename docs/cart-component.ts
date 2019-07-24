6a0e440405723dafedf36a2f4f7177c531414825
Kyle Andrews <kylea@page.works>
2019-07-24 08:40:39 -0400

    Testing moving imports into global functions declared via globals.d.ts

--- docs/cart-component.ts
+++ docs/cart-component.ts
@@ -1,4 +1,4 @@
-import uuid from 'uuid/v4';
+// import uuid from 'uuid/v4';
 
 class CartComponent extends HTMLElement
 {
--- docs/globals.d.ts
+++ docs/globals.d.ts
@@ -1,2 +1,4 @@
 declare module 'animejs';
 declare module 'uuid/v4';
+
+declare function uuid();
